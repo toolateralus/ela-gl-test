20aa7eb5dbbec57a5b1928503d81f3401473635d
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-02-24 19:07:03 -0500

    basic gl example for Ela language.

---
+++ shader/frag.glsl
@@ -0,0 +1,46 @@
+#version 450 core
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+const bool enableLighting = true;
+
+vec3 calculateLighting(vec3 normal, vec3 fragPos, vec3 lightPos, vec3 viewPos, vec3 lightColor, vec3 objectColor) {
+  // Ambient
+  float ambientStrength = 0.1;
+  vec3 ambient = ambientStrength * lightColor;
+  
+  // Diffuse 
+  vec3 norm = normalize(normal);
+  vec3 lightDir = normalize(lightPos - fragPos);
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuse = diff * lightColor;
+  
+  // Specular
+  float specularStrength = 0.5;
+  vec3 viewDir = normalize(viewPos - fragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  vec3 specular = specularStrength * spec * lightColor;
+  
+  return (ambient + diffuse + specular) * objectColor;
+}
+
+void main() {
+  vec3 result;
+  if (enableLighting) {
+    result = calculateLighting(Normal, FragPos, lightPos, viewPos, lightColor, objectColor);
+  } else {
+    result = objectColor;
+  }
+  FragColor = vec4(result, 1.0);
+  // FragColor = vec4(objectColor, 1.0);
+}
